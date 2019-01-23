867b5ab825a6ed48d873c8b5400cee54c2b694e2
Michael Kazman <michael.kazman@gmail.com>
2019-01-23 04:19:41 -0500

    modified block line comment search
    updated block line comment search

--- comment.py
+++ comment.py
@@ -1,7 +1,7 @@
 class Comment:
     # comment symbols to test for
     singleSymbols = ['#', '//'];
-    blockSymbols = ['/*'];
+    blockSymbols = ['/*', '//', '#']
     # constructor
     def __init__(self, text):
         # set text
@@ -17,12 +17,16 @@ class Comment:
 
     # checks if single line
     def isSingleComment(self):
+        # compares each symbol
         for symbol in self.singleSymbols:
+            # if contained, single comment occurs
             if symbol in self.text is not False : return True;
         return False;
 
     # checks if part of comment block
     def isBlockLine(self):
-        for symbol in self.singleSymbols:
-            if self.text.startswith(symbol) is True : return False;
-        return True;
+        for symbol in self.blockSymbols:
+            # as the text is stripped anyway, as long as the comment
+            # is not at the start, it is considered a "block" comment
+            if not self.text.startswith(symbol) and symbol in self.text[1:] : return True;
+        return False;
